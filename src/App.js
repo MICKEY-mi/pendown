17d021740ecd2484ef8be18a7d497f3b107a3db6
Mickey <mickeyth59@gmail.com>
2023-01-31 20:05:56 +0530

    update final

--- src/App.js
+++ src/App.js
@@ -2,25 +2,15 @@ import React from 'react';
 
 import './App.css';
 import Navbar from './components/navbar/navbar';
-import Navbarfirst from './components/navbar/navbar-first';
-import NavbarNote from './components/navbar/navbar_note';
 import Intro from './components/body/intro';
 import Footer from './components/footer/footer';
-import Firstyear from './components/firstyear/firstyear';
-import FirstyearLab from './components/firstyearlab/firstyearlab';
-import Notes from './components/Notesapp/notes';
 import { BrowserRouter , Routes, Route } from 'react-router-dom';
 function App() {
   return (
 
     <BrowserRouter>
       <Routes>
-          <Route index element={[(<Navbar />),(<Intro/>),(<Footer/>)]} />
-          <Route path="/firstyear" element={[(<Navbarfirst/>),(<Firstyear />)]} />
-          <Route path="/firstyear/lab" element={[(<Navbarfirst/>),(<FirstyearLab />)]} />
-          <Route path="*" element={[(<Navbar />),(<Intro/>),(<Footer/>)]} />
-          
-          <Route path="/note" element={[(<NavbarNote/>),(<Notes />)]} />
+          <Route path="*" element={[(<Navbar />),(<Intro/>),(<Footer/>)]} /> 
       </Routes>
     </BrowserRouter>
 
